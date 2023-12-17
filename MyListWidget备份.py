2c57753fe71fdcfa9c40eda5f5916306043d0030
PPJUST <106465946+ppjust@users.noreply.github.com>
2023-12-17 18:49:33 +0800

    清空仓库

--- MyListWidget备份.py
+++
@@ -1,9 +0,0 @@
-class MyListWidget(QListWidget):
-    itemMoved = Signal()
-
-    def __init__(self, parent=None):
-        super(MyListWidget, self).__init__(parent)
-
-    def dropEvent(self, event):
-        super(MyListWidget, self).dropEvent(event)
-        self.itemMoved.emit()
